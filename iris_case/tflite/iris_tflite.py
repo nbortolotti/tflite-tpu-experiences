640f93c2d308dc79b6b5cbdbc547f8db037c9957
Nicolas <nbortolotti@gmail.com>
2019-12-05 11:43:28 +0100

    Initial example using iris model dataset
    using iris dataset I created a  keras model, then exported, converted to tflite and  finally consumed from coral-device experience.

---
+++ iris_case/tflite/iris_tflite.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tflite_runtime.interpreter as tflite
+
+
+interpreter = tflite.Interpreter(model_path="converted_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Test model
+new_specie = np.array([7.9,3.8,6.4,2.0]) # general example to predict
+input_data = np.array(np.expand_dims(new_specie, axis=0), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
